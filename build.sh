17b40dcc8e80e4f15dc2e1ce2a8742d41b80212e
Ilya Voronin <ivoronin@gmail.com>
2020-10-21 16:51:40 +0300

    Add npm install to build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ else
 	CODE_SIGNING_ALLOWED="YES"
 fi
 
+npm i --python=python2.7
+
 rm -rf "${ARCHIVEPATH}"
 xcodebuild archive -project "${PROJECT}" -scheme "${SCHEME}" -configuration "${CONFIGURATION}" -archivePath "${ARCHIVEPATH}" CODE_SIGNING_ALLOWED="${CODE_SIGNING_ALLOWED}"
 [ "${CI}" ] || ./node_modules/.bin/create-dmg --overwrite "${ARCHIVEPATH}/Products/Applications/Tomato Bar.app"
