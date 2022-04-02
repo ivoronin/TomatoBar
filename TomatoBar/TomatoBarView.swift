7d468e6df8274e1d790c7765f597bac3151b9a62
Ilya Voronin <ivoronin@gmail.com>
2022-04-02 13:40:33 +0300

    Added quit and about buttons, fixes #16

--- TomatoBar/TomatoBarView.swift
+++ TomatoBar/TomatoBarView.swift
@@ -24,37 +24,89 @@ public struct TomatoBarView: View {
             .controlSize(.large)
             .keyboardShortcut(.defaultAction)
             Divider()
-            Toggle(isOn: $timer.stopAfterBreak) {
-                Text("Stop after break").frame(maxWidth: .infinity, alignment: .leading)
-            }.toggleStyle(.switch)
-            Toggle(isOn: $timer.showTimerInMenuBar) {
-                Text("Show timer in menu bar").frame(maxWidth: .infinity, alignment: .leading)
-            }
-            .onChange(of: timer.showTimerInMenuBar) { _ in
-                timer.renderTimeLeft()
-            }
-            .toggleStyle(.switch)
-            Stepper(value: $timer.workIntervalLength, in: 1 ... 60) {
-                Text("Work interval:").frame(maxWidth: .infinity, alignment: .leading)
-                Text("\(timer.workIntervalLength) min")
-            }
-            Stepper(value: $timer.restIntervalLength, in: 1 ... 60) {
-                Text("Rest interval:").frame(maxWidth: .infinity, alignment: .leading)
-                Text("\(timer.restIntervalLength) min")
+            Group {
+                Toggle(isOn: $timer.stopAfterBreak) {
+                    Text("Stop after break")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }.toggleStyle(.switch)
+                Toggle(isOn: $timer.showTimerInMenuBar) {
+                    Text("Show timer in menu bar")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }.toggleStyle(.switch)
+                    .onChange(of: timer.showTimerInMenuBar) { _ in
+                        timer.renderTimeLeft()
+                    }
+                Stepper(value: $timer.workIntervalLength, in: 1 ... 60) {
+                    Text("Work interval:")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("\(timer.workIntervalLength) min")
+                }
+                Stepper(value: $timer.restIntervalLength, in: 1 ... 60) {
+                    Text("Rest interval:")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("\(timer.restIntervalLength) min")
+                }
             }
             Divider()
-            Text("Sounds:")
-            HStack {
-                Toggle("Windup", isOn: $timer.isWindupEnabled)
-                Spacer()
-                Toggle("Ding", isOn: $timer.isDingEnabled)
-                Spacer()
-                Toggle("Ticking", isOn: $timer.isTickingEnabled)
-                    .onChange(of: timer.isTickingEnabled) { _ in
-                        timer.toggleTickingAction()
-                    }
+            Group {
+                Text("Sounds:")
+                HStack {
+                    Toggle("Windup", isOn: $timer.isWindupEnabled)
+                    Spacer()
+                    Toggle("Ding", isOn: $timer.isDingEnabled)
+                    Spacer()
+                    Toggle("Ticking", isOn: $timer.isTickingEnabled)
+                        .onChange(of: timer.isTickingEnabled) { _ in
+                            timer.toggleTickingAction()
+                        }
+                }
+            }
+            Divider()
+            Group {
+                Button {
+                    NSApp.activate(ignoringOtherApps: true)
+                    NSApp.orderFrontStandardAboutPanel()
+                } label: {
+                    Text("About")
+                    Spacer()
+                    Text("⌘ A").foregroundColor(Color.gray)
+                }
+                .buttonStyle(.plain)
+                .keyboardShortcut("a")
+                Button {
+                    NSApplication.shared.terminate(self)
+                } label: {
+                    Text("Quit")
+                    Spacer()
+                    Text("⌘ Q").foregroundColor(Color.gray)
+                }
+                .buttonStyle(.plain)
+                .keyboardShortcut("q")
             }
         }
-        .padding(12)
+        #if DEBUG
+            /*
+             After several hours of Googling and trying various StackOverflow
+             recipes I still haven't figured a reliable way to auto resize
+             popover to fit all it's contents (pull requests are welcome!).
+             The following code block is used to determine the optimal
+             geometry of the popover.
+             */
+            .overlay(
+                GeometryReader { proxy in
+                    debugSize(proxy: proxy)
+                }
+            )
+        #endif
+            /* Use values from GeometryReader */
+            .frame(width: 226, height: 267)
+            .padding(12)
     }
 }
+
+#if DEBUG
+    func debugSize(proxy: GeometryProxy) -> some View {
+        print("Optimal popover size:", proxy.size)
+        return Color.clear
+    }
+#endif
